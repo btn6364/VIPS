e27288ad782381d466ced214f21560a96aa669ce
Bao Nguyen <btn6364@gcis-vk3080-rl.main.ad.rit.edu>
2024-04-21 10:04:32 -0400

    Implement RTE

--- main/evaluation.py
+++ main/evaluation.py
@@ -1,2 +1,13 @@
 # TODO: Write some evalutation code here. 
+# TODO: Calculate RTE, RRE, latency and IoU. 
+import numpy as np 
 
+def rte(t1, t2): 
+    dist = np.linalg.norm(t1 - t2).round(2) 
+    return dist
+
+if __name__=="__main__": 
+    t1 = np.array([1,2,3])
+    t2 = np.array([1,1,1])
+    dist = rte(t1, t2)
+    print(f"Distance = {dist}")
