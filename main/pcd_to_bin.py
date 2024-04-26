5a580036880074afe190d3c7dc0c8df16310eeff
Bao Nguyen <btn6364@gcis-vk3080-rl.main.ad.rit.edu>
2024-04-25 21:10:19 -0400

    Update method to convert pcd to bin for test dataset

--- main/pcd_to_bin.py
+++ main/pcd_to_bin.py
@@ -1,47 +1,51 @@
 import numpy as np
 from pypcd4 import PointCloud
-import os
+import open3d as o3d
+import os 
 
-def pcd_to_bin(source_file, dataset, subdataset, filename, prefix):
-    pcd_data = PointCloud.from_path(source_file)
-    points = np.zeros([pcd_data.metadata.width, 4], dtype=np.float32)
-    points[:, 0] = pcd_data.pc_data['x'].copy()
-    points[:, 1] = pcd_data.pc_data['y'].copy()
-    points[:, 2] = pcd_data.pc_data['z'].copy()
-    points[:, 3] = pcd_data.pc_data['intensities'].copy().astype(np.float32)
-    
-    # Change .pcd file to .bin file
-    dest_filename = filename[:-4] + ".bin"
-    dest_file = f"../datasets/carla/{dataset}/{subdataset}/{prefix}_bin/{dest_filename}"
-    print(f"Saving {dest_file}")
-    with open(dest_file, 'wb') as f:
+def pcd_to_bin(pcd_file, tag):
+    # Read the PCD file
+    pcd = o3d.t.io.read_point_cloud(pcd_file)
+
+    # Get the positions and intensities
+    positions = pcd.point.positions
+    intensities = pcd.point.intensities
+
+    # Copy to a numpy array
+    points = np.zeros([positions.shape[0], 4], dtype=np.float32)
+    points[:, 0] = positions[:, 0].numpy()
+    points[:, 1] = positions[:, 1].numpy()
+    points[:, 2] = positions[:, 2].numpy()
+    intensities_copy = intensities.numpy().astype(np.float32).reshape((positions.shape[0],))
+    points[:, 3] = intensities_copy
+
+    # Save the binary point cloud
+    file_name = pcd_file.split("/")[-1][:-4]
+    dest_file = f"evaluation/{tag}_bin/{file_name}.bin"
+    print(f"Saving {dest_file}...")
+    with open(dest_file, "wb") as f:
         f.write(points.tobytes())
 
-def main():
-    # datasets = ["Dataset_1", "Dataset_2", "Dataset_3"]
-    datasets = ["Dataset_1"]
-    # subdatasets = ["D1", "D2", "D3", "D4", "D5"]
-    subdatasets = ["D1"]
-    for dataset in datasets:
-        for subdataset in subdatasets:
-            vehicle_pcd_dir = f"../datasets/carla/{dataset}/{subdataset}/vehicle_pcd/"
-            infra_pcd_dir = f"../datasets/carla/{dataset}/{subdataset}/infra_pcd/"
-            # Iterate over files in Vehicle_PCD
-            for filename in os.listdir(vehicle_pcd_dir):
-                f = os.path.join(vehicle_pcd_dir, filename)
-                pcd_to_bin(f, dataset, subdataset, filename, "vehicle")
-            
-            # Iterate over files in Infra_PCD
-            for filename in os.listdir(infra_pcd_dir):
-                f = os.path.join(infra_pcd_dir, filename)
-                pcd_to_bin(f, dataset, subdataset, filename, "infra")
+def convertAll():
+    vehicle_pcd_dir = "evaluation/Vehicle"
+    infra_pcd_dir = "evaluation/Infrastructure"
 
-if __name__=="__main__":
-    main()
-    # num_vehicle_pcd = len(os.listdir("../datasets/carla/Dataset_1/D1/vehicle_pcd"))
-    # num_vehicle_bin = len(os.listdir("../datasets/carla/Dataset_1/D1/vehicle_bin"))
-    # print(f"Num V_PCD = {num_vehicle_pcd}, V_BIN = {num_vehicle_bin}")
+    # Iterate over files in Vehicle_PCD
+    for filename in os.listdir(vehicle_pcd_dir):
+        f = os.path.join(vehicle_pcd_dir, filename)
+        pcd_to_bin(f, "vehicle")
+    
+    # Iterate over files in Infra_PCD
+    for filename in os.listdir(infra_pcd_dir):
+        f = os.path.join(infra_pcd_dir, filename)
+        pcd_to_bin(f, "infra")
 
-    # num_infra_pcd = len(os.listdir("../datasets/carla/Dataset_1/D1/infra_pcd"))
-    # num_infra_bin = len(os.listdir("../datasets/carla/Dataset_1/D1/infra_bin"))
-    # print(f"Num I_PCD = {num_infra_pcd}, I_BIN = {num_infra_bin}")
+if __name__=="__main__": 
+    convertAll()
+    # num_infra_pcds = len(os.listdir("evaluation/Infrastructure"))
+    # num_infra_bins = len(os.listdir("evaluation/infra_bin"))
+    # print(f"Num I_PCDs = {num_infra_pcds}, Num I_BINs = {num_infra_bins}")
+
+    # num_vehicle_pcds = len(os.listdir("evaluation/Vehicle"))
+    # num_vehicle_bins = len(os.listdir("evaluation/vehicle_bin"))
+    # print(f"Num V_PCDs = {num_vehicle_pcds}, Num V_BINs = {num_vehicle_bins}")
