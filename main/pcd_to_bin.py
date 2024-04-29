0f472a68d8b3fe616cde05308248907303fee01f
Bao Nguyen <btn6364@gcis-vk3080-rl.main.ad.rit.edu>
2024-04-29 08:38:21 -0400

    Update pcd_to_bin to convert tested point cloud

--- main/pcd_to_bin.py
+++ main/pcd_to_bin.py
@@ -9,26 +9,31 @@ def pcd_to_bin(pcd_file, tag):
 
     # Get the positions and intensities
     positions = pcd.point.positions
-    intensities = pcd.point.intensities
 
     # Copy to a numpy array
-    points = np.zeros([positions.shape[0], 4], dtype=np.float32)
+    points = np.zeros([positions.shape[0], 6], dtype=np.float32)
     points[:, 0] = positions[:, 0].numpy()
     points[:, 1] = positions[:, 1].numpy()
     points[:, 2] = positions[:, 2].numpy()
-    intensities_copy = intensities.numpy().astype(np.float32).reshape((positions.shape[0],))
-    points[:, 3] = intensities_copy
+
+    # Copy intensities
+    # intensities_copy = pcd.point.intensities.numpy().astype(np.float32).reshape((positions.shape[0],))
+
+    # Copy object_tag, cosAngle and object_id 
+    points[:, 3] = pcd.point.object_tag.numpy().astype(np.float32).reshape((positions.shape[0],))
+    points[:, 4] = pcd.point.cosAngle.numpy().astype(np.float32).reshape((positions.shape[0],))
+    points[:, 5] = pcd.point.object_id.numpy().astype(np.float32).reshape((positions.shape[0],))
 
     # Save the binary point cloud
     file_name = pcd_file.split("/")[-1][:-4]
-    dest_file = f"evaluation/{tag}_bin/{file_name}.bin"
+    dest_file = f"../Segmentation_Dataset/{tag}_bin/{file_name}.bin"
     print(f"Saving {dest_file}...")
     with open(dest_file, "wb") as f:
         f.write(points.tobytes())
 
 def convertAll():
-    vehicle_pcd_dir = "evaluation/Vehicle"
-    infra_pcd_dir = "evaluation/Infrastructure"
+    vehicle_pcd_dir = "../Segmentation_Dataset/Vehicle"
+    infra_pcd_dir = "../Segmentation_Dataset/Infrastructure"
 
     # Iterate over files in Vehicle_PCD
     for filename in os.listdir(vehicle_pcd_dir):
@@ -41,12 +46,12 @@ def convertAll():
         pcd_to_bin(f, "infra")
 
 if __name__=="__main__": 
-    # TODO: Always run python main/pcd_to_bin.py. Otherwise, it won't work. 
+    # pcd_to_bin("../Segmentation_Dataset/Vehicle/0.pcd", "vehicle")
     convertAll()
-    # num_infra_pcds = len(os.listdir("evaluation/Infrastructure"))
-    # num_infra_bins = len(os.listdir("evaluation/infra_bin"))
-    # print(f"Num I_PCDs = {num_infra_pcds}, Num I_BINs = {num_infra_bins}")
+    num_infra_pcds = len(os.listdir("../Segmentation_Dataset/Infrastructure"))
+    num_infra_bins = len(os.listdir("../Segmentation_Dataset/infra_bin"))
+    print(f"Num I_PCDs = {num_infra_pcds}, Num I_BINs = {num_infra_bins}")
 
-    # num_vehicle_pcds = len(os.listdir("evaluation/Vehicle"))
-    # num_vehicle_bins = len(os.listdir("evaluation/vehicle_bin"))
-    # print(f"Num V_PCDs = {num_vehicle_pcds}, Num V_BINs = {num_vehicle_bins}")
+    num_vehicle_pcds = len(os.listdir("../Segmentation_Dataset/Vehicle"))
+    num_vehicle_bins = len(os.listdir("../Segmentation_Dataset/vehicle_bin"))
+    print(f"Num V_PCDs = {num_vehicle_pcds}, Num V_BINs = {num_vehicle_bins}")
