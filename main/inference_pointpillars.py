367002f44b6c24362fe8e7b3409f0e2692e1ccfc
Bao Nguyen <btn6364@gcis-vk3080-rl.main.ad.rit.edu>
2024-04-30 08:04:15 -0400

    Finish logic for PointPillars Inference

--- main/inference_pointpillars.py
+++ main/inference_pointpillars.py
@@ -22,8 +22,8 @@ def runPointpillarsAll():
     # Start a timer
     start_time = time.time()
 
-    vehicle_bin_dir = f"../evaluation/vehicle_bin/"
-    infra_bin_dir = f"../evaluation/infra_bin/"
+    vehicle_bin_dir = f"../Segmentation_Dataset/vehicle_bin/"
+    infra_bin_dir = f"../Segmentation_Dataset/infra_bin/"
 
     # Iterate over files in Vehicle_BIN
     for filename in os.listdir(vehicle_bin_dir):
@@ -44,12 +44,12 @@ def runPointpillarsAll():
 
 if __name__=="__main__": 
     runPointpillarsAll()
-    # runPointpillarsOnePointCloud("../datasets/carla/Dataset_1/D1/infra_bin/1689811023.137300000.bin", "Dataset_1", "D1", "infra")
+    # runPointpillarsOnePointCloud("../Segmentation_Dataset/infra_bin/0.bin", "vehicle")
 
-    # num_vehicle_pcd = len(os.listdir("../datasets/carla/Dataset_1/D1/vehicle_pcd"))
-    # num_vehicle_predictions = len(os.listdir("../mmdetection3d/outputs/carla/Dataset_1/D1/vehicle/preds"))
+    # num_vehicle_pcd = len(os.listdir("../Segmentation_Dataset/Vehicle"))
+    # num_vehicle_predictions = len(os.listdir("../mmdetection3d/outputs/test/vehicle/preds"))
     # print(f"Num V_PCD = {num_vehicle_pcd}, V_PRED = {num_vehicle_predictions}")
 
-    # num_infra_pcd = len(os.listdir("../datasets/carla/Dataset_1/D1/infra_pcd"))
-    # num_infra_predictions = len(os.listdir("../mmdetection3d/outputs/carla/Dataset_1/D1/infra/preds"))
+    # num_infra_pcd = len(os.listdir("../Segmentation_Dataset/Infrastructure"))
+    # num_infra_predictions = len(os.listdir("../mmdetection3d/outputs/test/infra/preds"))
     # print(f"Num I_PCD = {num_infra_pcd}, I_PRED = {num_infra_predictions}")
